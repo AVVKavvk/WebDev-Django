51ba40e1050b9a6f34ac13481a48348333e48358
AVVKavvk <kumawatvipin066@gmail.com>
2024-12-10 17:04:10 +0530

    models and routing and tailwind

--- webDev/chai/models.py
+++ webDev/chai/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class ChaiVerity(models.Model):
+  CHAI_CHOICES = [
+    ("ML", "MASALA"),
+    ("GR", "GINGER"),
+    ("PL", "PLAIN"),
+    ("EL", "ELACHI"),
+    ("LE", "LEMON")
+  ]
+  name = models.CharField(max_length=100)
+  image = models.ImageField(upload_to="chais/")
+  date_added = models.DateTimeField(default=timezone.now())
+  chai_type = models.CharField(choices=CHAI_CHOICES, max_length=2)
+  
+  def __str__(self):
+    return self.name
+  
